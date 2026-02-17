a040aabd7f5c13368a1a4ddc9ffc60fb28d60142
Lu56-prog <lucerocardona167@gmail.com>
2026-02-17 18:42:38 -0500

    probando1

---
+++ proyect/src/main/java/app/ProyectLuceroApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectLuceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectLuceroApplication.class, args);
+	}
+
+}

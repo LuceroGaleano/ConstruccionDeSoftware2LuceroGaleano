4086aceedb7328df261d4efbe8302e059aca7e80
Lu56-prog <lucerocardona167@gmail.com>
2026-03-13 14:15:15 -0500

    Finish create classes 2.0

--- proyect/src/main/java/app/domain/models/User.java
+++ proyect/src/main/java/app/domain/models/User.java
@@ -1,5 +1,7 @@
 package app.domain.models;
 
+import java.sql.Date;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +15,7 @@ public class User extends Person{
     private long userID;
     private long relatedId;
     private String userName;
+    private Date brightdate;
     private RolUser systemRole;
     private String userStatus;
     private String password;
